7654a959f33e7c781b67845e3a8e846e50c4e717
Leonaldo <leonaldo.barbosa@gmail.com>
2020-03-03 21:00:13 -0300

    Aula 46 Criando um Pipe Puro

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import {registerLocaleData} from '@angular/common';
 import br from '@angular/common/locales/pt';
@@ -10,16 +11,19 @@ import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
 import { SettingsService } from './settings.service';
+import { FiltroArrayPipe } from './filtro-array.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
-    CamelCasePipe
+    CamelCasePipe,
+    FiltroArrayPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     SettingsService, 
